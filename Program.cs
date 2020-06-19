4a757c1480bb164669d38d6dc2a007c7d768c24d
Marcia Martins <marciapmartins@gmail.com>
2020-06-19 14:06:23 +0100

    Inclusao do projeto trabalhando com OO. Projeto que identifica os 10 estados brasileiros com maior extensao

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top10EstadosBrasileiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new Country();
+            var tops = states.Top10StatesByArea();
+
+            int cont = 0;
+            foreach(var top in tops)
+            {
+                Console.WriteLine($"{cont}: {top.Acronym}");
+                cont++;
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ State.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top10EstadosBrasileiros
+{
+    class State
+    {
+        public State(string name, string acronym)
+        {
+            this.Name = name;
+            this.Acronym = acronym;
+        }
+
+        public State(string name, string acronym, double territory): this(name, acronym)
+        {
+            this.Territory = territory;
+        }
+
+        public string Name { get; set; }
+        public string Acronym { get; set; }
+        public double Territory { get; set; }
+
+
+
+    }
+}
